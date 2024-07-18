cef7b3ae7afcf9b5988bcde208c8126305a61141
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-07-18 14:41:13 +0200

    Update chanel.js

--- commandes/chanel.js
+++ commandes/chanel.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "channel", reaction: "💫", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = 'Salut je m\'appelle *JOEL-MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
     let d = ' developpé par *joel_it🕸️🕷️*';
     let varmess = z + d;
-    var lien = 'https://whatsapp.com/channel/0029Vade9VgD38CPEnxfYF0M';  // Remplacez cet URL par le lien que vous souhaitez envoyer
+    var lien = 'https://chat.whatsapp.com/KStfBpcFDOi154eVtrz6n1';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
 });
 
@@ -17,7 +17,7 @@ console.log("mon test");
 console.log("mon test");
 /*module.exports.commande = () => {
   var nomCom = ["test","t"]
-  var reaction="☺️"
+  var reaction="💫"
   return { nomCom, execute,reaction }
 };
 
