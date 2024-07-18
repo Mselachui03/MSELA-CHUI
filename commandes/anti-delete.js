d3ac42927a5b57924243b655df3d4c5e7b6c1f90
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-07-18 14:19:48 +0200

    Update anti-delete.js

--- commandes/anti-delete.js
+++ commandes/anti-delete.js
@@ -7,7 +7,7 @@ let antiDeleteActive = false; // Variable pour stocker l'état de la commande an
 zokou({
   nomCom: "anti-delete",
   categorie: "General",
-  reaction: "🍑"
+  reaction: "🔰"
 }, async (origineMessage, zk, commandeOptions) => {
   const { ms, arg } = commandeOptions;
 
