683f4ca7f18e07e6ac012aa0a4b0fbd0e572c20c
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-07-18 15:47:25 +0200

    Update youtube.js

--- commandes/youtube.js
+++ commandes/youtube.js
@@ -30,7 +30,7 @@ zokou({
        
       let infoMess = {
   image: { url: videos[0].thumbnail },
-  caption: `╭━━joel md downloader menu━━╮
+  caption: `╭━━young africans md downloader menu━━╮
 *Song Name:* ${videos[0].title}
 
 *Uploaded:* ${videos[0].ago}
@@ -41,7 +41,7 @@ zokou({
 
 Views: ${videos[0].views}`,
         
-  whatsapp: "Join my WhatsApp channel: 'https://whatsapp.com/channel/0029Vade9VgD38CPEnxfYF0M'"
+  whatsapp: "Join my WhatsApp channel: 'https://chat.whatsapp.com/KStfBpcFDOi154eVtrz6n1'"
 };
      
 
@@ -179,7 +179,7 @@ zokou({
 
       let InfoMess = {
   image: { url: Element.thumbnail },
-  caption: `╭━━joel md download menu━━╮
+  caption: `╭━━young africans md download menu━━╮
 *Video Name:* ${Element.title}
 *Uploaded:* ${Element.ago}
 *Author:* ${Element.author.name}
@@ -211,7 +211,7 @@ _*Downloading...*_`
 
       fileStream.on('finish', () => {
         // Envoi du fichier vidéo en utilisant l'URL du fichier local
-        zk.sendMessage(origineMessage, { video: { url :"./video.mp4"} , caption: "*joel md", gifPlayback: false }, { quoted: ms });
+        zk.sendMessage(origineMessage, { video: { url :"./video.mp4"} , caption: "*young africans md", gifPlayback: false }, { quoted: ms });
       });
 
       fileStream.on('error', (error) => {
